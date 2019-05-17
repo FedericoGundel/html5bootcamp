3bbd6b37125132de76cb8285ef97e481c6b47eee
Federico Gundel <fede.gundel@gmailcom>
2019-05-17 08:39:06 -0300

    Topic 1 Exercise 3 reusable function added

--- Module_1/js/index.js
+++ Module_1/js/index.js
@@ -1,18 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(e) {
   console.log("DOM fully loaded and parsed");
   load();
-  });
+});
 function load(){
-  mostrar("hidden");
-  var botonAlert =document.getElementById("alert");
-  console.log(botonAlert);
-  botonAlert.addEventListener("click", function(e) {
-    alert("alert");
+  mostrar();
+  var botonInsert = document.getElementById("insert");
+  botonInsert.addEventListener("click", function(){
+    var miInit = { "method": 'GET',
+                   "headers": {
+              			'Content-Type': 'application/json'
+              		 },
+                   "mode": 'cors',
+                   "cache": 'default'};
+    url = "http://api.icndb.com/jokes/random";
+    insertarData(url,miInit);
   });
+  console.log("bien");
+  }
+
+
+function insertarData(url,miInit) {
+  try {
+  fetch(url,{ "method": miInit.method,
+                 "headers": miInit.headers[0],
+                 "mode": miInit.mode,
+                 "cache": miInit.cache }).then(function(response){
+    response.json().then(function(json){
+      msj = json.value.joke;
+      let selector = document.getElementById("hidden");
+      selector.innerHTML = "<p>"+msj+"</p>";
+      console.log("html reemplazado");
+    })
+  });
+  }catch (e) {
+  console.log("pepe");
+  let selector = document.getElementById("hidden");
+  console.log("hola");
+  selector.classList.add("colorRed");
+  }
 }
 
-function mostrar(id){
-  let selector = document.getElementById(id);
-  console.log(selector);
-  selector.style.display="block";
+
+function mostrar(){
+  let selector = document.getElementById("hidden");
+  selector.classList.remove("hide");
 }
