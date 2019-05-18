1c338f703cfc554495eaa8aa1715f6c32d3ce074
Federico Gundel <fede.gundel@gmailcom>
2019-05-18 10:20:53 -0300

    Module 1/Exercise 1 & 2: repos fetch and validation markup added

--- Module_1/js/index.js
+++ Module_1/js/index.js
@@ -4,9 +4,14 @@ document.addEventListener("DOMContentLoaded", (e)=> {
 });
 function load(){
   mostrar();
-  var botonInsert = document.getElementById("insert");
+  let botonSearch = document.getElementById("search");
+  botonSearch.addEventListener("click",()=>{
+    let name = document.getElementById("name").value;
+    fetchRepos(name);
+  });
+  let botonInsert = document.getElementById("insert");
   botonInsert.addEventListener("click", ()=>{
-    var miInit = { "method": 'GET',
+    let miInit = { "method": 'GET',
                    "headers": {
               			'Content-Type': 'application/json'
               		 },
@@ -41,7 +46,26 @@ function insertarData(url,miInit) {
 
 
 }
-
+function fetchRepos(name){
+  fetch("https://api.github.com/search/repositories?q='"+name+"'",{
+          "method": 'GET',
+          "headers": {
+          'Content-Type': 'application/json'
+          },
+          "mode": 'cors',
+          "cache": 'default'}).then((response)=>{
+            response.json().then((repoJson)=>{
+              let selec = document.getElementById("RepoList");
+              let list;
+              list="<h2>Repositorios que coinciden con ese nombre:</h2><ul>";
+              for(repo of repoJson.items){
+                list+="<li>"+repo.full_name+"</li>";
+              }
+              list+="</ul>";
+              selec.innerHTML = list;
+            })
+          })
+}
 
 function mostrar(){
   let selector = document.getElementById("hidden");
