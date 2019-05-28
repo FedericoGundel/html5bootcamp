30c626702aa6c70aa2b4832bb2b8e2b7db08821d
Federico Gundel <fede.gundel@gmail.com>
2019-05-28 19:56:31 -0300

    [Module_4][Exercise 1] added errase function using LocalStorage an IndexedDB

--- Module_4/src/app.js
+++ Module_4/src/app.js
@@ -1,23 +1,14 @@
-//almacen de datos con LocalStorage
+
 let miStorage = window.localStorage;
 
-document.getElementById("save").addEventListener("click",saveDocument);
-
-function saveDocument(){
-    let text=  document.getElementById("document").value;
-    miStorage.setItem("text",text);
-    console.log("texto almacenado:"+miStorage.getItem("text"));
-}
-
-//almacen de datos con IndexedDB
 let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-
 
 window.onload = () =>{
     let db = null;
     StartDB();
     document.getElementById("save").addEventListener("click",save);
-} 
+    document.getElementById("errase").addEventListener("click",errase);
+}
 function StartDB(){
     db = indexedDB.open("MyTestDatabase", 4);
     db.onupgradeneeded= (e) => {
@@ -26,10 +17,10 @@ function StartDB(){
         store = active.createObjectStore("documents", {
              keyPath : "id",autoIncremet:true
         });
-        store.createIndex('by_title', 'title', {
+        store.createIndex('by_id', 'id', {
             unique: false
         });
-        
+
     };
 
     db.onerror = (e) => {
@@ -39,17 +30,26 @@ function StartDB(){
     db.onsuccess = (e) => {
     console.log("data base opened");
     }
-    
+
 }
 
 function save(){
+
+    //almacen de datos con LocalStorage
+
+    let text=  document.getElementById("document").value;
+    miStorage.setItem("text",text);
+    console.log("texto almacenado:"+miStorage.getItem("text"));
+
+    //almacen de datos con IndexedDB
+
     let active = db.result;
     let data = active.transaction(["documents"], "readwrite");
     let store = data.objectStore("documents");
 
     let request = store.put({
-        id:0,
-        title: document.getElementById("document").value
+        id:1,
+        text: document.getElementById("document").value
     });
 
     request.onerror = (e) => {
@@ -59,6 +59,26 @@ function save(){
     data.oncomplete = (e) => {
         console.log("object succesfully saved");
     }
-   
-}
 
+}
+function errase(){
+
+    //borrando dato usando LocalStorage
+
+    localStorage.removeItem("text");
+
+    //borrando dato usando IndexedDB
+
+    let active = db.result;
+    let data = active.transaction(["documents"], "readwrite");
+    let store = data.objectStore("documents");
+    let request = store.delete(1);
+
+    data.oncomplete = (event) => {
+        console.log('Transaction not opened due to error: ' + data.error);
+    }
+    request.onerror = (e) => {
+        console.log(request.error.name +" "+request.error.message);
+    }
+
+}
