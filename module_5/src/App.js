b9cdd127aa731855942da3fd4045447ac2de0f04
Federico Gundel <fede.gundel@gmailcom>
2019-06-05 22:30:16 -0300

    [Module_5][Exercise 1] AddMovie added

--- module_5/src/App.js
+++ module_5/src/App.js
@@ -2,48 +2,44 @@ import React, { Component } from 'react';
 import MovieList from './components/MovieList.js'
 import './App.css';
 
-/*let movies = [{
-  id:1,
-  title:"Avengers",
-  genre:"Accion"
-}, {
-  id:2,
-  title:"Avengers",
-  genre:"Accion"
-}, {
-  id:3,
-  title:"Avengers",
-  genre:"Accion"
-}]*/
-
-
 
 class App extends Component{
   constructor(){
     super()
     this.state = {
-      movies : [{
-        id:1,
-        title:"Avengers",
-        genre:"Accion"
-      }, {
-        id:2,
-        title:"Spiderman",
-        genre:"Accion"
-      }, {
-        id:3,
-        title:"Hulk",
-        genre:"Accion"
-      }]
+      movies : []
     }
+    //this.handleClick = this.handleClick.bind(this);
+    
+    
+  }
+  
+  addMovie(e){
+    e.preventDefault();
+    const newMovie = {
+      id:this.state.movies.length,
+      title:this.newTitle.value,
+      genre:this.newGenre.value
+    }
+    this.setState({
+      movies : [...this.state.movies, newMovie]
+    })
   }
   render(){
-
+    console.log(this.state.movies.length)
     return (
-
+      
       <div className="App">
         <h1>Movies List</h1>
         <MovieList movies = {this.state.movies}/>
+        <form onSubmit={(e)=>{this.addMovie(e)}}>
+          <div>
+            <label>Add a new Movie:</label>
+            <input ref={(input)=>{this.newTitle=input}} type="text" id ="title" placeholder="title"></input>
+            <input ref={(input)=>{this.newGenre=input}} type="text" id ="genre" placeholder="genre"></input>
+            <button type="submit">Add</button>
+          </div>          
+        </form>
       </div>
     );
   }
